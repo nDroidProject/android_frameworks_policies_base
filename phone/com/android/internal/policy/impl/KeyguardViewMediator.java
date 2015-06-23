b237767e2017ba6ebb4af74ba23b1c8bc07ce044
Josh Max <joshumax@hotmail.com>
2015-06-22 22:28:09 -0700

    Disable lock screen after boot

--- phone/com/android/internal/policy/impl/KeyguardViewMediator.java
+++ phone/com/android/internal/policy/impl/KeyguardViewMediator.java
@@ -473,7 +473,7 @@ public class KeyguardViewMediator implements KeyguardViewCallback,
             }
             
             if (DEBUG) Log.d(TAG, "doKeyguard: showing the lock screen");
-            showLocked();
+            //showLocked();
         }
     }
 
